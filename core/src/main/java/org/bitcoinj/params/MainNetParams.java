2fb6501507989d84f9803880dd6245491ce6700e
mint-young <mintyoungs2@gmail.com>
2021-02-22 11:55:36 +0900

    Test7

--- core/src/main/java/org/bitcoinj/params/MainNetParams.java
+++ core/src/main/java/org/bitcoinj/params/MainNetParams.java
@@ -40,7 +40,7 @@ public class MainNetParams extends AbstractBitcoinNetParams {
         addressHeader = 0;
         p2shHeader = 5;
         segwitAddressHrp = "bc";
-        port = 8333;
+        port = 9777;
         packetMagic = 0xf9beb4d9L;
         bip32HeaderP2PKHpub = 0x0488b21e; // The 4 byte header that serializes in base58 to "xpub".
         bip32HeaderP2PKHpriv = 0x0488ade4; // The 4 byte header that serializes in base58 to "xprv"
