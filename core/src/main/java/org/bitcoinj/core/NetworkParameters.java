02780c758c2819841abff3b3b478273adf570250
mint-young <mintyoungs2@gmail.com>
2021-02-22 11:53:31 +0900

    Test6

--- core/src/main/java/org/bitcoinj/core/NetworkParameters.java
+++ core/src/main/java/org/bitcoinj/core/NetworkParameters.java
@@ -136,8 +136,8 @@ public abstract class NetworkParameters {
         return genesisBlock;
     }
 
-    public static final int TARGET_TIMESPAN = 14 * 24 * 60 * 60;  // 1 hour per difficulty cycle, on average.
-    public static final int TARGET_SPACING = 10 * 60;  // 1 minutes per block.
+    public static final int TARGET_TIMESPAN = 60 * 60;  // 1 hour per difficulty cycle, on average.
+    public static final int TARGET_SPACING = 1 * 60;  // 1 minutes per block.
     public static final int INTERVAL = TARGET_TIMESPAN / TARGET_SPACING;
     
     /**
